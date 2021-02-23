3f088fcab8b579f95c3fd5a4f461aff6a6ddac15
Hubtwork <hubtwork@gmail.com>
2021-02-24 00:02:43 +0900

    feature/ui : ADD Images from URL with Cache
    ADD Images from URl with Cache
    ADD Image Preview Test
    
    TODO - Grid Item List with Thubmnail Image for Shopping List

---
+++ Panda-Saza/Panda-Saza/Models/ShopItem/ItemThumbnail.swift
@@ -0,0 +1,23 @@
+//
+//  ItemThumbnail.swift
+//  Panda-Saza
+//
+//  Created by Jae Heo on 2021/02/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct ItemThumbnail: Hashable, Codable {
+    var id: Int
+    var itemName: String
+    var itemNameTrans: String
+    var itemPrice: Int
+    
+    var registrationTime: Int
+    
+    var sellerLoc: String
+    var interest: Int
+    
+    var thumbnailImageURL: String
+}
---
+++ Panda-Saza/Panda-Saza/View/ShoppingItem/ItemThumbnailView.swift
@@ -0,0 +1,20 @@
+//
+//  ItemThumbnailView.swift
+//  Panda-Saza
+//
+//  Created by Jae Heo on 2021/02/23.
+//
+
+import SwiftUI
+
+struct ItemThumbnailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ItemThumbnailView_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemThumbnailView()
+    }
+}
